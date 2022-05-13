850e672d9415d6c5b6518d1c9f7b7ef219b2954e
mqthis12 <fildooogaming@gmail.com>
2022-05-13 22:12:50 +0200

    login

--- src/main/java/noWaste/controller/AppController.java
+++ src/main/java/noWaste/controller/AppController.java
@@ -10,11 +10,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @Controller
-public class AppController {
+public class AppController  extends WebMvcConfigurerAdapter {
 
     @Autowired
     private UserRepository userRepo;
